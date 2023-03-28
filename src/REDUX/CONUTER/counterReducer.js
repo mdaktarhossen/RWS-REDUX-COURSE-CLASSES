d5730dc79cfef8dd194f158afdbe998380363d22
mdaktarhossen <mdaktarhossen20@gmail.com>
2023-03-28 11:39:06 +0600

    OK

---
+++ src/REDUX/CONUTER/counterReducer.js
@@ -0,0 +1,25 @@
+import { DECREMENT, INCREMENT } from "./actionType";
+
+const initalState = {
+  value: 0
+}
+const counterReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        value:state.value +1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        value:state.value -1,
+      };
+    default:
+      return {
+        state
+      }
+  }
+};
+
+export default counterReducer;
